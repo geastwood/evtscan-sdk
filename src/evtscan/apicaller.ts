314ef9b65f9bf5384975feed7f0aa481a9a1e9d5
NHibiki <i@yuuno.cc>
2019-06-11 12:06:16 +0000

    Add: api list for evtscan

--- src/evtscan/apicaller.ts
+++ src/evtscan/apicaller.ts
@@ -11,5 +11,16 @@ export default class ApiCaller {
         if (timeout) this.timeout = timeout;
 
     }
+    
+    async get(uri: String, params={}, headers={}) {
+        return Axios.get(this.entrypoint + uri, {
+            params,
+            headers: {
+                'User-Agent': 'EvtScanSDK v1.0',
+                ...headers
+            },
+            timeout: this.timeout
+        });
+    }
 
 }
--- src/evtscan/evtscan.ts
+++ src/evtscan/evtscan.ts
@@ -6,18 +6,58 @@ export interface EvtScanConfig {
     debug?: Boolean;
 }
 
+export interface EvtScanParams {
+    since?: String;
+    from?: String;
+    page?: Number = 0;
+    size?: Number = 10;
+}
+
 export default class EvtScan {
 
     private entrypoint: String = "https://evtscan.io";
     private timeout: Number = 3000;
     private debug: Boolean = false;
     private apiCaller: ApiCaller;
+    
+    private defaultParams: EvtScanParams;
+    
+    public static R = {
+        General: {
+            System: '/',
+            ChainInfo: '/chaininfo',
+            Address: '/searchAddress'
+        },
+        Recent: {
+            Block: '/block',
+            Transaction: '/transaction',
+            Everipay: ['/trxByName', {trx_name: 'everipay'}],
+            Everipass: ['/trxByName', {trx_name: 'everipass'}],
+            Action: '/action',
+            Fungible: '/fungible',
+            Domain: '/domain',
+            Group: '/group',
+            Nonfungible: '/nonfungible'
+        },
+        Detail: {
+            Block: '/block/%%',
+            Transaction: '/transaction/%%',
+            Fungible: '/fungible/%%',
+            Domain: '/domain/%%',
+            Group: '/group/%%',
+            Nonfungible: '/nonfungible/%%',
+            Address: '/address/%%',
+            AddressAssets: '/addressAssets/%%',
+            AddressHistory: '/addressHistory/%%'
+        }
+    };
 
-    constructor(config: EvtScanConfig) {
+    constructor(config: EvtScanConfig, defaultParams?: EvtScanParams) {
 
         if (config.entrypoint) this.entrypoint = config.entrypoint;
         if (config.timeout) this.timeout = config.timeout;
         if (config.debug) this.debug = config.debug;
+        if (defaultParams) this.defaultParams = defaultParams;
 
         this.apiCaller = new ApiCaller(this.entrypoint, this.timeout);
 
